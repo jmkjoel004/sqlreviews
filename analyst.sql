8ec515654bf206a11215bbc81f75a967d25d54fb
jmkjoel004 <137641654+jmkjoel004@users.noreply.github.com>
2024-05-15 14:31:10 -0400

    Create analyst.sql

---
+++ analyst.sql
@@ -0,0 +1,20 @@
+CREATE TABLE celebraties_Tours (
+  id INTEGER PRIMARY KEY NOT NULL,
+  name TEXT,
+  nationality CHAR(15) NOT NULL,
+  stageName TEXT NOT NULL,
+  age INTEGER,
+  typeOfMusic TEXT,
+  firstAlbum TEXT,
+  lastAlbum TEXT,
+  africaTours VARCHAR(3),
+  numberOfAfricaTours INTEGER,
+  americaTours VARCHAR(3),
+  numberOfamericaTours INTEGER,
+  asiatours TEXT,
+  numberOfAsiatours VARCHAR(3),
+  EuropeTours TEXT,
+  numberOfEuropeTours VARCHAR(3),
+  austaliaTours TEXT,
+  numberOfAustaliaTours VARCHAR(3)
+  );
